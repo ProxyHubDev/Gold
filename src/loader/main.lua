1ae1e2b63bcb7e380ca3c7c42a0f49206ea567dd
Proxy <proxyhubofc@gmail.com>
2026-07-06 15:36:15 -0300

    Update main.lua

--- src/loader/main.lua
+++ src/loader/main.lua
@@ -1,1 +1,11 @@
+local Loaders = {
+    [135858844777165] = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/ProxyHubDev/Gold/refs/heads/main/src/scripts/animeduelists.lua"))()
+    end
+}
 
+local Loader = Loaders[game.PlaceId]
+
+if Loader then
+    Loader()
+end
