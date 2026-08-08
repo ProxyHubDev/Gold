a99d6834b242b8866405fd63df74b0b18d25a68e
Proxy <proxyhubofc@gmail.com>
2026-08-08 19:56:55 -0300

    Update main.lua

--- src/loader/main.lua
+++ src/loader/main.lua
@@ -1,15 +1,13 @@
-local Loaders = {
-    [94717504417144] = function()
-        loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/6f41d36a79b4b144394beff23b6e3433cb6a72e9629d4f72f2dceb2cbbec49c1/download"))()
-    end,
-
-    [135858844777165] = function()
-        loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/4e04c52a2de9e4f203a6148796e6f4c3ef029951f1cb10e8a17cf5be29f0c9e2/download"))()
-    end
+local loaders = {
+    [135858844777165] = "77ba2e4bade838b8aa12826e16054674", -- Anime Duelist
+    [94717504417144] = "ca9cf8e4bd3332a329d960da48ac9f1a", -- Anime Capture
 }
 
-local Loader = Loaders[game.PlaceId]
+local loaderId = loaders[game.PlaceId]
 
-if Loader then
-    Loader()
+if not loaderId then
+    warn("game is not supported")
+    return
 end
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/" .. loaderId .. ".lua"))()
